367d1b7d29ebffc3300565188dfa6ec165d826fb
Uvrak <danielhorn101@gmail.com>
2020-06-11 07:00:10 +0200

    SDL_window created and input for leaving the program

--- main.c
+++ main.c
@@ -1,19 +1,88 @@
 #include <stdio.h>
 #include <SDL.h>
+#include "constants.h"
 
-void initializeWindow() {
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL; 
 
+int isGameRunning = FALSE;
+
+int initializeWindow() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "Error initializing SDL.\n");
+		return FALSE;
+	}
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		SDL_WINDOW_BORDERLESS
+	);
+
+	if (!window) {
+		fprintf(stderr, "Error creating SDL window .\n");
+		return FALSE;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		fprintf(stderr, "Error creating SDL renderer.\n");
+		return FALSE;
+	}
+
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+	return TRUE;
 }
 
-int main() {
-	initializeWindow();
+void destroyWindow() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
 
-	//setup();
+void setup() {
+	// TODO:
+	// initialize and setup game objects
+}
 
-	while (1) {
-		//processInput();
-		//update();
-		//render();
+void processInput() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type) {
+		case SDL_QUIT: {
+			isGameRunning = FALSE;
+			break;
+		}
+		case SDL_KEYDOWN: {
+			if (event.key.keysym.sym == SDLK_ESCAPE) {
+				isGameRunning = FALSE;
+			}
+			break;
+		}
 	}
+}
+void render() {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	// TODO:
+	// render all game objects for the current frame
+
+	SDL_RenderPresent(renderer);
+}
+
+int main(int argc, char* argv[]) {
+	isGameRunning = initializeWindow();
+
+	setup();
+
+	while (isGameRunning) {
+		processInput();
+		//update();
+		render();
+	}
+	destroyWindow();
 	return 0;
 }
