c9580cd1c8f0e79050971fbc3f836be681ffa20f
Uvrak <danielhorn101@gmail.com>
2020-06-11 05:58:49 +0200

    added SDL Files

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <SDL.h>
+
+void initializeWindow() {
+
+}
+
+int main() {
+	initializeWindow();
+
+	//setup();
+
+	while (1) {
+		//processInput();
+		//update();
+		//render();
+	}
+	return 0;
+}
